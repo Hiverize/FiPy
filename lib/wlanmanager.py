b25bff3b866cde025c4cf0065b62e7ec74cd7579
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-10-10 11:22:35 +0200

    restructure wlanmanager

--- lib/wlanmanager.py
+++ lib/wlanmanager.py
@@ -1,18 +1,24 @@
-import network
-from config import Config
-import time
-import machine
 import binascii
+import machine
+import network
+import time
+import sys
+
+from config import Config
 
 class WLanManager():
 
     def __init__(self):
         self.config = Config()
+        self.wlan = network.WLAN()
 
-    def scan(self, wlan):
+    def scan(self):
+        self.wlan.deinit()
+        time.sleep(1)
+
         # Scan to find all available SSIDs
-        wlan.init(mode=network.WLAN.STA)
-        scan = wlan.scan()
+        self.wlan.init(mode=network.WLAN.STA)
+        scan = self.wlan.scan()
         ssids = [{
             'ssid': s.ssid,
             'bssid': binascii.hexlify(s.bssid).decode('utf-8'),
@@ -20,10 +26,10 @@ class WLanManager():
             'channel': s.channel} for s in scan]
         self.config.set_value('networking', 'wlan', 'available', ssids)
         self.config.write()
+        self.wlan.deinit()
         return len(ssids)
 
-    def enable_ap(self, default=False):
-        wlan = network.WLAN(id=0)
+    def enable_ap(self):
 
         # Resolve mode to its numeric code
         mode = network.WLAN.AP
@@ -33,38 +39,76 @@ class WLanManager():
         encryption = self.config.get_value('networking', 'accesspoint', 'encryption')
         channel = self.config.get_value('networking', 'accesspoint', 'channel')
 
-        wlan.init(mode=mode,
-                  ssid=ssid,
-                  auth=(encryption, password),
-                  channel=channel)
-        wlan.ifconfig(id=1,
-                      config=('192.168.4.1',
-                              '255.255.255.0',
-                              '192.168.4.1',
-                              '192.168.4.1'))
-        time.sleep(10)
+        try:
+            self.wlan.deinit()
+            time.sleep(1)
+            self.wlan.init(mode=mode,
+                           ssid=ssid,
+                           auth=(encryption, password),
+                           channel=channel)
+        except:
+            print("WLan restart failed!")
+            raise
+
+        try:
+            self.wlan.ifconfig(id=1,
+                               config=('192.168.4.1',
+                                       '255.255.255.0',
+                                       '192.168.4.1',
+                                       '192.168.4.1'))
+        except:
+            print("WLan ifconfig failed!")
+            raise
+        else:
+            time.sleep(5)
 
     def enable_client(self):
+
         # Resolve mode to its numeric code
         mode = network.WLAN.STA
-
-        wlan = network.WLAN(mode=mode)
-        time.sleep(2)
 
         ssid = self.config.get_value('networking', 'wlan', 'ssid') 
         password = self.config.get_value('networking', 'wlan', 'password')
         encryption = int(self.config.get_value('networking', 'wlan', 'encryption'))
-        wlan.connect(ssid,
-                     auth=(encryption, password))
-        time.sleep(10)
 
-        # ifconfig = self.config.get_value('networking', 'wlan', 'ifconfig')
-        # if ifconfig == 'dhcp':
-        #     wlan.ifconfig(config='dhcp')
-        # else:
-        #     ipaddress = self.config.get_value('networking', 'wlan', 'ipaddress')
-        #     subnet = self.config.get_value('networking', 'wlan', 'subnet')
-        #     gateway = self.config.get_value('networking', 'wlan', 'gateway')
-        #     dns = self.config.get_value('networking', 'wlan', 'dns')
-        #     ip_config=(ipaddress, subnet, gateway, dns)
-        #     wlan.ifconfig(config=ip_config)
+        if not (ssid and encryption and password):
+            print("No WLan connection configured!")
+            return
+
+        try:
+            self.wlan.deinit()
+            time.sleep(1)
+            self.wlan.init(mode=mode)
+        except:
+            print("WLan restart failed!")
+            raise
+
+
+        try:
+            ifconfig = self.config.get_value('networking', 'wlan', 'ifconfig')
+            if ifconfig == 'static':
+                ipaddress = self.config.get_value('networking', 'wlan', 'ipaddress')
+                subnet = self.config.get_value('networking', 'wlan', 'subnet')
+                gateway = self.config.get_value('networking', 'wlan', 'gateway')
+                dns = self.config.get_value('networking', 'wlan', 'dns')
+
+                ip_config = (ipaddress, subnet, gateway, dns)
+                self.wlan.ifconfig(id=0, config=ip_config)
+            else:
+                self.wlan.ifconfig(id=0, config='dhcp')
+        except:
+            print("WLan ifconfig failed!")
+            raise
+
+        try:
+            self.wlan.connect(ssid,
+                              auth=(encryption, password),
+                              timeout=5000)
+        except:
+            print("WLan connect failed!")
+            print("SSID: {}, Enc: {}, PSK: {}".format(ssid,
+                                                      encryption,
+                                                      password))
+            raise
+        else:
+            time.sleep(5)
