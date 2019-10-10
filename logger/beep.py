4bf2eb7c87e4f9e8294dff5f1526842939b26858
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-10-10 11:30:13 +0200

    add exception handling for beep logger

--- logger/beep.py
+++ logger/beep.py
@@ -1,12 +1,19 @@
 import urequests
+import sys
 
 class Beep:
     def __init__(self, config):
         self.server = config.get_value('telemetry', 'beep', 'server')
         self.key = config.get_value('telemetry', 'beep', 'sensor_key')
+        self.failures = 0
 
     def add(self, data):
         data['key'] = self.key
-        urequests.post(
-            '{}/api/sensors'.format(self.server),
-            json=data)
+        try:
+            urequests.post(
+                '{}/api/sensors'.format(self.server),
+                json=data)
+        except:
+            self.failures += 1
+            print("Error sending data! (Count: {:d})".format(self.failures))
+            print(sys.exc_info())
