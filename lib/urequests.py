ae2982ae80e47f8d4f7f50a421267af7b72dc1b6
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-10-11 09:34:54 +0200

    add performance information and catch empty response

--- lib/urequests.py
+++ lib/urequests.py
@@ -1,3 +1,4 @@
+import machine
 import usocket
 
 class Response:
@@ -33,11 +34,17 @@ class Response:
 
 
 def request(method, url, data=None, json=None, headers={}, stream=None):
+    perf = machine.Timer.Chrono()
+    perf.start()
+
     try:
         proto, dummy, host, path = url.split("/", 3)
     except ValueError:
         proto, dummy, host = url.split("/", 2)
         path = ""
+    except AttributeError as e:
+        print(e)
+        return 
     if proto == "http:":
         port = 80
     elif proto == "https:":
@@ -49,16 +56,25 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
     if ":" in host:
         host, port = host.split(":", 1)
         port = int(port)
+    prep_time = perf.read_ms()
 
     #ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
-    ai = ai = usocket.getaddrinfo(host, port)
+
+    ai = usocket.getaddrinfo(host, port)
     ai = ai[0]
 
     s = usocket.socket(ai[0], ai[1], ai[2])
+    s.setblocking(True)
+    s.settimeout(5)
+
+    sock_init_time = perf.read_ms() - prep_time
     try:
         s.connect(ai[-1])
+        sock_connect_time = perf.read_ms() - sock_init_time
         if proto == "https:":
             s = ussl.wrap_socket(s, server_hostname=host)
+        sock_ssl_time = perf.read_ms() - sock_connect_time
+
         s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
         if not "Host" in headers:
             s.write(b"Host: %s\r\n" % host)
@@ -75,36 +91,50 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
             s.write(b"Content-Type: application/json\r\n")
         if data:
             s.write(b"Content-Length: %d\r\n" % len(data))
+        s.write(b"Connection: close\r\n")
         s.write(b"\r\n")
+        sock_header_time = perf.read_ms() - sock_ssl_time
+        
         if data:
             s.write(data)
+        sock_data_time = perf.read_ms() - sock_header_time
 
         l = s.readline()
-        #print(l)
-        l = l.split(None, 2)
-        status = int(l[1])
-        reason = ""
-        if len(l) > 2:
-            reason = l[2].rstrip()
+        if l is not None:
+            l = l.split(None, 2)
+            status = int(l[1])
+            reason = ""
+            if len(l) > 2:
+                reason = l[2].rstrip()
+        else:
+            print("No response")
+            status = 0
+            reason = ""
         while True:
             l = s.readline()
             if not l or l == b"\r\n":
                 break
-            #print(l)
+
             if l.startswith(b"Transfer-Encoding:"):
                 if b"chunked" in l:
                     raise ValueError("Unsupported " + l)
             elif l.startswith(b"Location:") and not 200 <= status <= 299:
                 raise NotImplementedError("Redirects not yet supported")
+        sock_response_time = perf.read_ms() - sock_data_time
     except OSError:
         s.close()
         raise
 
-    s.close()
+    print("Prep: {:.0f}ms Init: {:.0f}ms Connect: {:.0f}ms SSL: {:.0f}ms "
+          "Header: {:.0f}ms Data: {:.0f}ms Response: {:.0f}ms ".format(
+              prep_time, sock_init_time, sock_connect_time, sock_ssl_time,
+              sock_header_time, sock_data_time, sock_response_time
+          ))
 
     resp = Response(s)
     resp.status_code = status
     resp.reason = reason
+    resp.close()
     return resp
 
 
