2a6a7ff2f18549c4ce98149c697d13f758d08137
diren <diren@uni-bremen.de>
2020-03-11 14:41:50 +0100

    added didis csv-write function as well

--- logger/csv.py
+++ logger/csv.py
@@ -158,4 +158,113 @@ class CSV_logger:
             f.write(full_string)
         print("Wrote 1 line to csv at {}".format(full_time_string))
 
-    
+      """ Datenfile yyyy-mm-dd.csv mit time, value1, ... value10               """
+    def add_data_didi(self, data, plt, cycle):
+        date_string = self.get_date()
+        time_string = self.get_time()
+        hour_string = self.get_hour()
+        file_path = self.dir + "/" + date_string +  "x" + hour_string +  ".csv"
+        file_path = self.dir + "/" + date_string +  ".csv"
+        # Get Time
+        time_string, full_time_string = self.get_time_string()
+        # combine dict entries
+        # data_list = ["{},  {},  {}\n".format(full_time_string, key, value) for key, value in data.items()]
+        # print('data_list:', data_list)
+        full_time_string = date_string + ',  ' + self.get_time()
+
+        test_list = ' '
+        test_list = test_list +  str(cycle) + ', '
+
+        test_list = test_list + full_time_string + ',  '
+        if 't' in data:
+            test_list = test_list +  str(data['t']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 'p' in data:
+            test_list = test_list +  str(data['p']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 'h' in data:
+            test_list = test_list +  str(data['h']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 'weight_kg' in data:
+            test_list = test_list +  str(data['weight_kg']) + ',  '
+        else:
+            test_list = test_list + ',  '
+
+        if 't_i_1' in data:
+            # t1  = data['t_i_1']    # t1s = ("{:5.1f}".format(t1))
+            test_list = test_list +  str(data['t_i_1']) + ', '
+        else:
+            test_list = test_list + ', '
+#        print('add_data 3')
+        if 't_i_2' in data:
+            test_list = test_list +  str(data['t_i_2']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 't_i_3' in data:
+            test_list = test_list +  str(data['t_i_3']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 't_i_4' in data:
+            test_list = test_list +  str(data['t_i_4']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 't_i_5' in data:
+            test_list = test_list +  str(data['t_i_5']) + ', '
+        else:
+            test_list = test_list + ', '
+        if 't_o'   in data:
+            test_list = test_list +  str(data['t_o']) + ', '
+        else:
+            test_list = test_list + ', '
+
+        test_list = test_list     + ' \n'
+        print('   SD-Card:', test_list, end = ' ')
+
+        # Write header, if file did not exist before
+        try:
+            f = open(file_path, 'r')
+            f.close()
+        except OSError:
+            f = open(file_path, 'w')
+            f.write('Cycle,,       Time,               BME280,,,               HX711,    DS18B20 \n')
+            f.write('Cycle, Date,         Time,        t,     p,        h,       kg,     t1,     t2,     t3,     t4,     t5,     to \n')
+            f.close()
+            print("   Logging measurements to " + file_path)
+        # Append Value
+        with open(file_path, 'a') as f:
+            # f.write("".join(data_list))
+            f.write("".join(test_list))
+        print('  Datei:   Daten in {}'.format(file_path))
+
+        # Plotfile für gnuplot yyyy-mm-dd.plt erzeugen
+        if plt:
+            file_path = self.dir + "/" + date_string +  ".plt"
+            try:
+                f = open(file_path, 'r')
+                f.close()
+            except OSError:
+                f = open(file_path, 'w')
+                # f.write('# Plotfile  2020-02-08.plt am 08.02.2020 um 00:00:00 \n')
+                text = '# Plotfile  ' + file_path + ' am ' + self.get_date() + ' ab ' + self.get_time() + ' \n'
+                f.write(text)
+                text = 'DATAFILE = "' + self.get_date() + '.csv" \n'
+                f.write(text)
+                text = 'TITLE    = "Messdaten der BOB-Bienenbeute' + ' am ' + self.get_date() + ' ab ' + self.get_time() + '" \n'
+                f.write(text)
+                f.write('load       "PlotBOB.plt" \n')
+                f.write('       \n')
+                f.write('# exit \n')
+                f.write('pause -1 "Details Gewicht"         \n')
+                f.write('set xrange ["10:00:00":"12:00:00"] \n')
+                f.write('set yrange [0:10] \n')
+                f.write('replot \n')
+                f.write('       \n')
+                f.write('pause -1 "Details Temperatur"      \n')
+                f.write('set xrange ["10:00:00":"12:00:00"] \n')
+                f.write('set yrange [15:25] \n')
+                f.write('replot \n')
+                f.close()
+                print("   Logging measurements to " + file_path)
